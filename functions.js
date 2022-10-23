21bac08cfccf4b07a37bf965f0173ae4675ae5f9
ewhites <ejwhite43@tntech.edu>
2022-10-23 07:57:57 -0500

    First Smodel

---
+++ functions.js
@@ -0,0 +1,203 @@
+let correct_1 = document.getElementById('correct1');
+correct_1.addEventListener('click', function(){
+correct_1.style.background = '#00FF00';
+})
+let wrong1_1 = document.getElementById('wrong1.1');
+wrong1_1.addEventListener('click', function(){
+wrong1_1.style.background = '#FF0000';
+})
+let wrong1_2 = document.getElementById('wrong1.2');
+wrong1_2.addEventListener('click', function(){
+wrong1_2.style.background = '#FF0000';
+})
+let wrong1_3 = document.getElementById('wrong1.3');
+wrong1_3.addEventListener('click', function(){
+wrong1_3.style.background = '#FF0000';
+})
+
+let correct_2 = document.getElementById('correct2');
+correct_2.addEventListener('click', function(){
+correct_2.style.background = '#00FF00';
+})
+let wrong2_1 = document.getElementById('wrong2.1');
+wrong2_1.addEventListener('click', function(){
+wrong2_1.style.background = '#FF0000';
+})
+let wrong2_2 = document.getElementById('wrong2.2');
+wrong2_2.addEventListener('click', function(){
+wrong2_2.style.background = '#FF0000';
+})
+let wrong2_3 = document.getElementById('wrong2.3');
+wrong2_3.addEventListener('click', function(){
+wrong2_3.style.background = '#FF0000';
+})
+
+let correct_3 = document.getElementById('correct3');
+correct_3.addEventListener('click', function(){
+correct_3.style.background = '#00FF00';
+})
+let wrong3_1 = document.getElementById('wrong3.1');
+wrong3_1.addEventListener('click', function(){
+wrong3_1.style.background = '#FF0000';
+})
+let wrong3_2 = document.getElementById('wrong3.2');
+wrong3_2.addEventListener('click', function(){
+wrong3_2.style.background = '#FF0000';
+})
+let wrong3_3 = document.getElementById('wrong3.3');
+wrong3_3.addEventListener('click', function(){
+wrong3_3.style.background = '#FF0000';
+})
+
+let correct_4 = document.getElementById('correct4');
+correct_4.addEventListener('click', function(){
+correct_4.style.background = '#00FF00';
+})
+let wrong4_1 = document.getElementById('wrong4.1');
+wrong4_1.addEventListener('click', function(){
+wrong4_1.style.background = '#FF0000';
+})
+let wrong4_2 = document.getElementById('wrong4.2');
+wrong4_2.addEventListener('click', function(){
+wrong4_2.style.background = '#FF0000';
+})
+let wrong4_3 = document.getElementById('wrong4.3');
+wrong4_3.addEventListener('click', function(){
+wrong4_3.style.background = '#FF0000';
+})
+
+let correct_5 = document.getElementById('correct5');
+correct_5.addEventListener('click', function(){
+correct_5.style.background = '#00FF00';
+})
+let wrong5_1 = document.getElementById('wrong5.1');
+wrong5_1.addEventListener('click', function(){
+wrong5_1.style.background = '#FF0000';
+})
+let wrong5_2 = document.getElementById('wrong5.2');
+wrong5_2.addEventListener('click', function(){
+wrong5_2.style.background = '#FF0000';
+})
+let wrong5_3 = document.getElementById('wrong5.3');
+wrong5_3.addEventListener('click', function(){
+wrong5_3.style.background = '#FF0000';
+})
+
+let correct_6 = document.getElementById('correct6');
+correct_6.addEventListener('click', function(){
+correct_6.style.background = '#00FF00';
+})
+let wrong6_1 = document.getElementById('wrong6.1');
+wrong6_1.addEventListener('click', function(){
+wrong6_1.style.background = '#FF0000';
+})
+let wrong6_2 = document.getElementById('wrong6.2');
+wrong6_2.addEventListener('click', function(){
+wrong6_2.style.background = '#FF0000';
+})
+let wrong6_3 = document.getElementById('wrong6.3');
+wrong6_3.addEventListener('click', function(){
+wrong6_3.style.background = '#FF0000';
+})
+
+let correct_7 = document.getElementById('correct7');
+correct_7.addEventListener('click', function(){
+correct_7.style.background = '#00FF00';
+})
+let wrong7_1 = document.getElementById('wrong7.1');
+wrong7_1.addEventListener('click', function(){
+wrong7_1.style.background = '#FF0000';
+})
+let wrong7_2 = document.getElementById('wrong7.2');
+wrong7_2.addEventListener('click', function(){
+wrong7_2.style.background = '#FF0000';
+})
+let wrong7_3 = document.getElementById('wrong7.3');
+wrong7_3.addEventListener('click', function(){
+wrong7_3.style.background = '#FF0000';
+})
+
+let correct_8 = document.getElementById('correct8');
+correct_8.addEventListener('click', function(){
+correct_8.style.background = '#00FF00';
+})
+let wrong8_1 = document.getElementById('wrong8.1');
+wrong8_1.addEventListener('click', function(){
+wrong8_1.style.background = '#FF0000';
+})
+let wrong8_2 = document.getElementById('wrong8.2');
+wrong8_2.addEventListener('click', function(){
+wrong8_2.style.background = '#FF0000';
+})
+let wrong8_3 = document.getElementById('wrong8.3');
+wrong8_3.addEventListener('click', function(){
+wrong8_3.style.background = '#FF0000';
+})
+
+let correct_9 = document.getElementById('correct9');
+correct_9.addEventListener('click', function(){
+correct_9.style.background = '#00FF00';
+})
+let wrong9_1 = document.getElementById('wrong9.1');
+wrong9_1.addEventListener('click', function(){
+wrong9_1.style.background = '#FF0000';
+})
+let wrong9_2 = document.getElementById('wrong9.2');
+wrong9_2.addEventListener('click', function(){
+wrong9_2.style.background = '#FF0000';
+})
+let wrong9_3 = document.getElementById('wrong9.3');
+wrong9_3.addEventListener('click', function(){
+wrong9_3.style.background = '#FF0000';
+})
+
+let correct_10 = document.getElementById('correct10');
+correct_10.addEventListener('click', function(){
+correct_10.style.background = '#00FF00';
+})
+let wrong10_1 = document.getElementById('wrong10.1');
+wrong10_1.addEventListener('click', function(){
+wrong10_1.style.background = '#FF0000';
+})
+let wrong10_2 = document.getElementById('wrong10.2');
+wrong10_2.addEventListener('click', function(){
+wrong10_2.style.background = '#FF0000';
+})
+let wrong10_3 = document.getElementById('wrong10.3');
+wrong10_3.addEventListener('click', function(){
+wrong10_3.style.background = '#FF0000';
+})
+
+let correct_11 = document.getElementById('correct11');
+correct_11.addEventListener('click', function(){
+correct_11.style.background = '#00FF00';
+})
+let wrong11_1 = document.getElementById('wrong11.1');
+wrong11_1.addEventListener('click', function(){
+wrong11_1.style.background = '#FF0000';
+})
+let wrong11_2 = document.getElementById('wrong11.2');
+wrong11_2.addEventListener('click', function(){
+wrong11_2.style.background = '#FF0000';
+})
+let wrong11_3 = document.getElementById('wrong11.3');
+wrong11_3.addEventListener('click', function(){
+wrong11_3.style.background = '#FF0000';
+})
+
+let correct_12 = document.getElementById('correct12');
+correct_12.addEventListener('click', function(){
+correct_12.style.background = '#00FF00';
+})
+let wrong12_1 = document.getElementById('wrong12.1');
+wrong12_1.addEventListener('click', function(){
+wrong12_1.style.background = '#FF0000';
+})
+let wrong12_2 = document.getElementById('wrong12.2');
+wrong12_2.addEventListener('click', function(){
+wrong12_2.style.background = '#FF0000';
+})
+let wrong12_3 = document.getElementById('wrong12.3');
+wrong12_3.addEventListener('click', function(){
+wrong12_3.style.background = '#FF0000';
+})
